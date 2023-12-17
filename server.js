7a2e373a2387947386f02ba87239187299c34219
Watxher <104895198+watxherthedumbass@users.noreply.github.com>
2023-12-17 20:05:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,42 @@ var app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded( {extended: false})
 app.use(express.static('site'));
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'jar83957@gmail.com',
+    pass: 'kla3yjarr3n18'
+  }
+});
+
 
 app.get('/', function (req, res) {
    res.sendFile('index.html');
 })
 
 app.post('/', urlencodedParser, function (req, res) {
+   mailOptions = {
+   from: 'jar83957@gmail.com',
+   to: 'jar83957@gmail.com',
+   subject: `review from ${req.body.name}`,
+   text: `review: ${req.body.txt}`
+};
    response = {
-      name:req.body.name
+      name:req.body.name,
+      txt:req.body.txt
    };
    console.log(response);
-   res.send(`hello ${req.body.name}`);
+   transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+   res.send(`review sent sucessfully from ${req.body.name}`);
+   
 })
 
 var server = app.listen(8080, function () {
