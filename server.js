4f8001c1c491af1098d82019bbd45ec3b34bcc60
Watxher <104895198+watxherthedumbass@users.noreply.github.com>
2023-12-17 12:36:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
-
+var parser = require('body-parser');
+var encoded = parser.urlencoded( {extended: false})
 app.use(express.static('site'));
 
 app.get('/', function (req, res) {
    res.send('Hello World');
+})
+
+app.post('/process-post', encoded, function (req, res) {
+   response = {
+      name:req.body.name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
 })
 
 var server = app.listen(8080, function () {
